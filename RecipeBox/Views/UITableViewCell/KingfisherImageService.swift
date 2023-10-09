addc502c9f71c98564ddeb4ec9df81175ffdaa12
Elena <lenarusskih@gmail.com>
2023-10-09 14:51:57 +0300

    Add image placeholder for images in recipes

--- RecipeBox/Views/UITableViewCell/KingfisherImageService.swift
+++ RecipeBox/Views/UITableViewCell/KingfisherImageService.swift
@@ -13,18 +13,21 @@ protocol ImageServiceProtocol {
 }
 
 final class KingfisherImageService: ImageServiceProtocol {
-    func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
-        if let url = URL(string: urlString) {
-            KingfisherManager.shared.retrieveImage(with: url) { result in
-                switch result {
-                case .success(let value):
-                    completion(value.image)
-                case .failure:
-                    completion(nil)
-                }
-            }
-        } else {
-            completion(nil)
-        }
-    }
+  func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
+      let placeholderImage = UIImage(named: "dish_placeholder")
+         if let url = URL(string: urlString) {
+           let resource = KF.ImageResource(downloadURL: url)
+             let option: KingfisherOptionsInfo = [.transition(.fade(0.2))]
+             KingfisherManager.shared.retrieveImage(with: resource, options: option) { result in
+                 switch result {
+                 case .success(let value):
+                     completion(value.image)
+                 case .failure:
+                     completion(placeholderImage)
+                 }
+             }
+         } else {
+             completion(placeholderImage)
+         }
+     }
 }
