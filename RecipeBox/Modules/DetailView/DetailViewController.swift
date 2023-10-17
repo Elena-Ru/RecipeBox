3f849efcce98a7a822f96f80f0239e8b457d7f02
Elena <lenarusskih@gmail.com>
2023-10-17 08:23:10 +0300

    Extract RecipeImageCell

--- RecipeBox/Modules/DetailView/DetailViewController.swift
+++ RecipeBox/Modules/DetailView/DetailViewController.swift
@@ -85,38 +85,3 @@ extension DetailViewController {
         case main
     }
 }
-
-// MARK: - RecipeImageCell
-class RecipeImageCell: UICollectionViewCell {
-    static let identifier = "RecipeImageCell"
-    
-    var recipeImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = 20
-        imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupViews() {
-        addSubview(recipeImageView)
-        
-        NSLayoutConstraint.activate([
-            recipeImageView.topAnchor.constraint(equalTo: topAnchor),
-            recipeImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            recipeImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            recipeImageView.trailingAnchor.constraint(equalTo: trailingAnchor)
-        ])
-    }
-
-}
